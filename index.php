f7f14d563af7d264717320974d0589cca4d57e58
Riski Febriansyah <riski-febriansyah@outlook.com>
2015-08-10 12:02:40 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+#index include template, koneksi database dan fungsi
+
+<?php
+
+include "config/config.php";
+include "include/fungsi.php";
+
+// Template
+
+include "header.php";
+include "switch/switch.php";
+include "footer.php";
+
+// end Template
+
+?>
